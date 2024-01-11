69562967ab5e491c8d4a2cedce88386d73628b1c
ViEmMo <vittorio.morella@gmail.com>
2024-01-11 13:50:14 +0100

    Create _.py

---
+++ _.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+breast_cancer = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data",
+                           names=["id","diagnosis","radius_mean","texture_mean","perimeter_mean","area_mean","smoothness_mean","compactness_mean","concavity_mean","concave points_mean","symmetry_mean","fractal_dimension_mean","radius_se","texture_se","perimeter_se","area_se","smoothness_se","compactness_se","concavity_se","concave points_se","symmetry_se","fractal_dimension_se","radius_worst","texture_worst","perimeter_worst","area_worst","smoothness_worst","compactness_worst","concavity_worst","concave points_worst","symmetry_worst","fractal_dimension_worst"])
+
+breast_cancer.head()
+
+X=breast_cancer.drop(['diagnosis','id'],axis=1).values
+Y=breast_cancer['diagnosis'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size=0.3, random_state=0)
+
+le=LabelEncoder()
+y_train=le.fit_transform(y_train)
+y_test=le.fit_transform(y_test)
+
+ss=StandardScaler()
+X_train=ss.fit_transform(X_train)
+X_test=ss.fit_transform(X_test)
+
+model = Sequential()
+model.add(Dense(12, input_dim=X_train.shape[1], activation='relu'))
+model.add(Dense(4, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
+
+model.summary()
+
+model.fit(X_train,y_train, epochs=100)
